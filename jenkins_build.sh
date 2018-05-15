a13ca3721a23c0341af1527cc724cecf278eea35
n-tange <ntange0@gmail.com>
2018-05-15 16:53:18 +0900

    jenkinsシェルにタグ付けを追加

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -13,7 +13,17 @@ ERR=$?
 if [ $ERR -eq 0 ]; then
   IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} jenkins_identidock_1)
   CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
-  if [ $CODE -ne 200 ]; then
+  if [ $CODE -eq 200 ]; then
+    echo "Test passed - Tagging"
+    HASH=$(git rev-parse --short HEAD)
+    sudo docker tag jenkins_identidock mydockerreg:5000/identidock:$HASH
+    sudo docker tag jenkins_identidock mydockerreg:5000/identidock:newest
+    echo "Pushing"
+    #localのDockerレジストリ（mydockerreg）へ
+    sudo docker push mydockerreg:5000/identidock:$HASH
+    sudo docker push mydockerreg:5000/identidock:newest
+
+  else
     echo "Site returned " $CODE
     ERR=1
   fi
